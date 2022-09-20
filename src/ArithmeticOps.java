b7fd19fb36a5f4995d781789290ed9734d7d066d
Hitesh Mailarapwar <hitesh@accrualify.com>
2022-09-20 12:06:09 +0530

    changes

---
+++ src/ArithmeticOps.java
@@ -0,0 +1,8 @@
+package src;
+
+public interface ArithmeticOps {
+    public void Addition(int No1, int No2);
+    public void Substraction(int No1, int No2);
+    public void Multiplication(int No1, int No2);
+}
+//changes
